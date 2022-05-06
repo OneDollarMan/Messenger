ea73b67d8692ceb6d199632d524e1f128c539a01
vladislav <vladskorikov@gmail.com>
2022-05-06 13:01:40 +0400

    Added index page

--- src/main/java/rgr/Messenger/MessengerApplication.java
+++ src/main/java/rgr/Messenger/MessengerApplication.java
@@ -1,9 +1,16 @@
 package rgr.Messenger;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"rgr.Messenger.Controller", "rgr.Messenger.Configuration", "rgr.Messenger.Service"})
+@EnableJpaRepositories(basePackages ={"rgr.Messenger.Repository"})
+@EntityScan(basePackages ={"rgr.Messenger.Entity"})
 public class MessengerApplication {
 
 	public static void main(String[] args) {
